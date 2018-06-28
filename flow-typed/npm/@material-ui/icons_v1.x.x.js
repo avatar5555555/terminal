3718801142fd42485e6a5c10b1471d1a5d7af2bf
Aleksey Sergeev <avatar5555555@gmail.com>
2018-06-28 11:57:44 +0400

    Add flow-type fro material icons

---
+++ flow-typed/npm/@material-ui/icons_v1.x.x.js
@@ -0,0 +1,26 @@
+// @flow
+
+declare module '@material-ui/icons' {
+  declare module.exports: React$ComponentType<{
+    color?: 'action' | 'disabled' | 'error',
+    nativeColor?: string,
+    titleAccess?: string,
+    viewBox?: string
+  }>
+}
+
+declare module '@material-ui/icons/ArrowBack' {
+  declare module.exports: $Exports<'@material-ui/icons'>
+}
+
+declare module '@material-ui/icons/Menu' {
+  declare module.exports: $Exports<'@material-ui/icons'>
+}
+
+declare module '@material-ui/icons/Payment' {
+  declare module.exports: $Exports<'@material-ui/icons'>
+}
+
+declare module '@material-ui/icons/Send' {
+  declare module.exports: $Exports<'@material-ui/icons'>
+}
