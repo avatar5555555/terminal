5790b0b025c6e27cb3507e26cefee1a9a06353ee
Aleksey Sergeev <avatar5555555@gmail.com>
2018-06-29 21:41:55 +0400

    Remove MenuButton

--- src/Components/Bar/Bar.js
+++ src/Components/Bar/Bar.js
@@ -1,66 +1,43 @@
 // @flow
 
-import React, { Component } from 'react'
+import React from 'react'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import IconButton from '@material-ui/core/IconButton'
 import ArrowBack from '@material-ui/icons/ArrowBack'
+import MenuIcon from '@material-ui/icons/Menu'
 import { FormattedMessage } from 'react-intl'
-
-import MenuButton from '../MenuButton'
 
 import type { BarProps } from 'src/types'
 
-type State = {
-  anchorEl: HTMLElement | null
-}
+const Bar = ({ classes, pathname, goBack }: BarProps) => {
+  const isRoot = pathname === '/'
 
-class Bar extends Component<BarProps, State> {
-  state = {
-    anchorEl: null
-  }
+  return (
+    <AppBar position="static" color="default" className={classes.root}>
+      <Toolbar>
+        {isRoot ? (
+          <IconButton id="menu-button" color="inherit" aria-label="Menu">
+            <MenuIcon />
+          </IconButton>
+        ) : (
+          <IconButton
+            id="back-button"
+            color="inherit"
+            aria-label="Menu"
+            onClick={goBack}
+          >
+            <ArrowBack />
+          </IconButton>
+        )}
 
-  handleOpen = (e: { currentTarget: HTMLElement }) =>
-    this.setState({ anchorEl: e.currentTarget })
-
-  handleClose = () => this.setState({ anchorEl: null })
-
-  render() {
-    const { classes, pathname, goBack } = this.props
-    const isRoot = pathname === '/'
-    const { anchorEl } = this.state
-
-    return (
-      <AppBar position="static" color="default" className={classes.root}>
-        <Toolbar>
-          {isRoot ? (
-            <MenuButton
-              aria-owns={anchorEl ? 'simple-menu' : null}
-              aria-haspopup="true"
-              handleOpen={this.handleOpen}
-              handleClose={this.handleClose}
-              anchor={anchorEl}
-              id="menu-button"
-            />
-          ) : (
-            <IconButton
-              id="back-button"
-              color="inherit"
-              aria-label="Menu"
-              onClick={goBack}
-            >
-              <ArrowBack />
-            </IconButton>
-          )}
-
-          <Typography variant="title" color="inherit">
-            <FormattedMessage id="Title" />
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    )
-  }
+        <Typography variant="title" color="inherit">
+          <FormattedMessage id="Title" />
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  )
 }
 
 export default Bar
--- src/Components/Bar/index.test.js
+++ src/Components/Bar/index.test.js
@@ -53,39 +53,6 @@ describe('DisconnectedBar', () => {
 
     expect(wrapper.find('#menu-button').length).toBe(1)
   })
-
-  it('checks aria-owns prop if anchorEl is set', () => {
-    const wrapper = wrap({ ...props })
-
-    const buttonProps = wrapper
-      .setState({ anchorEl: 'HTMLElement' })
-      .find('#menu-button')
-      .props()
-
-    expect(buttonProps['aria-owns']).toBe('simple-menu')
-  })
-
-  it('it set anchorEl on menu click and drop on select', () => {
-    const wrapper = shallow(<Bar {...props} />)
-
-    expect(wrapper.state('anchorEl')).toBe(null)
-
-    wrapper
-      .find('#menu-button')
-      .dive()
-      .find('[aria-label="Menu"]')
-      .simulate('click', { currentTarget: 'wow' })
-
-    expect(wrapper.state('anchorEl')).toBe('wow')
-
-    wrapper
-      .find('#menu-button')
-      .dive()
-      .find('#menu-item')
-      .simulate('click')
-
-    expect(wrapper.state('anchorEl')).toBe(null)
-  })
 })
 
 describe('functions', () => {
