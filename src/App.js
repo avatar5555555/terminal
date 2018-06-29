1c9636d985bf470d125d9b1657ac33e79dc4153c
Aleksey Sergeev <avatar5555555@gmail.com>
2018-06-29 16:51:15 +0400

    Move providers from index to App

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
 // @flow
+
 import React, { Fragment } from 'react'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import { IntlProvider } from 'react-intl'
+import { BrowserRouter } from 'react-router-dom'
+import { MuiThemeProvider } from '@material-ui/core/styles'
+import { Provider } from 'react-redux'
+
+import './index.css'
 
 import Router from 'src/router'
 import Bar from 'src/Components/Bar'
+import messages from 'src/i18n'
+import config from 'src/config'
+import theme from 'src/services/theme'
+import store from 'src/store'
+
+const { locale } = config
 
 const App = () => (
-  <Fragment>
-    <Bar />
-    <Router />
-    <ToastContainer />
-  </Fragment>
+  <IntlProvider
+    locale={locale}
+    defaultLocale={locale}
+    messages={messages[locale]}
+  >
+    <BrowserRouter>
+      <MuiThemeProvider theme={theme}>
+        <Provider store={store}>
+          <Fragment>
+            <Bar />
+            <Router />
+            <ToastContainer />
+          </Fragment>
+        </Provider>
+      </MuiThemeProvider>
+    </BrowserRouter>
+  </IntlProvider>
 )
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,37 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { IntlProvider } from 'react-intl'
-import { BrowserRouter } from 'react-router-dom'
-import { MuiThemeProvider } from '@material-ui/core/styles'
-import { Provider } from 'react-redux'
-
-import './index.css'
 
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
-import messages from 'src/i18n'
-import config from 'src/config'
-import theme from 'src/services/theme'
-import store from 'src/store'
-
-const { locale } = config
-
-const renderApp = () => (
-  <IntlProvider
-    locale={locale}
-    defaultLocale={locale}
-    messages={messages[locale]}
-  >
-    <BrowserRouter>
-      <MuiThemeProvider theme={theme}>
-        <Provider store={store}>
-          <App />
-        </Provider>
-      </MuiThemeProvider>
-    </BrowserRouter>
-  </IntlProvider>
-)
-
-ReactDOM.render(renderApp(), document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
