9cd2763302b19fe5b221e009af56a2c9a6bd38cd
Aleksey Sergeev <avatar5555555@gmail.com>
2018-06-29 16:54:38 +0400

    Move Bar to Bar.js

--- src/Components/Bar/index.js
+++ src/Components/Bar/index.js
@@ -1,86 +1,26 @@
 // @flow
-import React, { Component } from 'react'
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
-import Typography from '@material-ui/core/Typography'
-import IconButton from '@material-ui/core/IconButton'
-import ArrowBack from '@material-ui/icons/ArrowBack'
+
 import { withStyles } from '@material-ui/core/styles'
-import { FormattedMessage } from 'react-intl'
 import { compose } from 'redux'
 import { connect } from 'react-redux'
 import { push } from 'connected-react-router'
 
-import MenuButton from '../MenuButton'
+import Bar from './Bar'
 
-type Props = {
-  classes: { root: string },
-  pathname: string,
-  back: Function
-}
+import type { Theme, State } from 'src/types'
 
-type State = {
-  anchorEl: HTMLElement | null
-}
-
-export const styles = theme => ({
+export const styles = (theme: Theme) => ({
   root: {
     marginBottom: theme.spacing.unit
   }
 })
 
-export class Bar extends Component<Props, State> {
-  state = {
-    anchorEl: null
-  }
-
-  handleOpen = e => this.setState({ anchorEl: e.currentTarget })
-
-  handleClose = () => this.setState({ anchorEl: null })
-
-  render() {
-    const { classes, pathname, back } = this.props
-    const isRoot = pathname === '/'
-    const { anchorEl } = this.state
-
-    return (
-      <AppBar position="static" color="default" className={classes.root}>
-        <Toolbar>
-          {isRoot ? (
-            <MenuButton
-              aria-owns={anchorEl ? 'simple-menu' : null}
-              aria-haspopup="true"
-              handleOpen={this.handleOpen}
-              handleClose={this.handleClose}
-              anchor={anchorEl}
-              id="menu-button"
-            />
-          ) : (
-            <IconButton
-              id="back-button"
-              color="inherit"
-              aria-label="Menu"
-              onClick={back}
-            >
-              <ArrowBack />
-            </IconButton>
-          )}
-
-          <Typography variant="title" color="inherit">
-            <FormattedMessage id="Title" />
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    )
-  }
-}
-
-const mapState = state => ({
+const mapState = (state: State) => ({
   pathname: state.router.location.pathname
 })
 
-export const bindActions = dispatch => ({
-  back: () => dispatch(push('/'))
+export const bindActions = (dispatch: Function) => ({
+  goBack: () => dispatch(push('/'))
 })
 
 export default compose(
